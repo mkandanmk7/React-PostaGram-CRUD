2cf772d2fe13543427df150cd70d125adb32bda3
muthu <mkandanmk7@gmail.com>
2021-09-24 12:14:07 +0530

    post sTarted

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h2 className="btn btn-dark ">Welcome to Postagram</h2>
+    </div>
+  );
+}
+
+export default App;
